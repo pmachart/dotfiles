c0c0c5b125ce88ffcf497e568b3ce749c653c357
Pierre-Marie Achart <pierremarie.achart@gmail.com>
2018-08-09 09:51:10 +0200

    master : new job, new tmux

--- tmuxinit.sh
+++ tmuxinit.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-tmux new-session -d -s ofs -c '/home/pma/git/pegasus'
+if [[ $MYENV == 't460p' ]] ; then
 
-tmux send-keys 'git willpull ; g' 'C-m'
+tmux new-session -d -s wynd -c '/home/pma/git/mellisuge'
+
+tmux send-keys 'tb' 'C-m'
 tmux select-window -t ofs:1
 
-tmux split-window -v -c "#{pane_current_path}" -p 15
-tmux send-keys 'yarn dev:client:build' 'C-m'
+tmux split-window -h -c "#{pane_current_path}" -p 20
+tmux send-keys 'dcdev' 'C-m'
 
-tmux split-window -h -t 2 -c "#{pane_current_path}"
-tmux send-keys 'yarn dev:server:prepare && yarn dev:server:build' 'C-m'
+tmux split-window -v -t 2 -c "#{pane_current_path}" -p 6
+tmux send-keys 'dcw' 'C-m'
 
-tmux split-window -h -t 3 -c "#{pane_current_path}"
-tmux send-keys 'tf logs/serverConsole.log' 'C-m'
+tmux split-window -h -t 1 -c "#{pane_current_path}" -p 40
+tmux send-keys 'git willpull ; g' 'C-m'
 
-tmux split-window -h -t 1 -c "#{pane_current_path}" -p 10
-tmux send-keys 'sleep 85' 'C-m' 'yarn dev:server:start' 'C-m'
+tmux split-window -v -t 1 -c "#{pane_current_path}" -p 4
+tmux send-keys 'yesterday'
 
-tmux select-pane 1
-tmux attach -t ofs
+#tmux select-pane -t 2
+tmux attach -t wynd
+
+else
+
+tmux
+
+fi
+
