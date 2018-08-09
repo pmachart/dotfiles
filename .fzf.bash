dfc16bb90671f154bffa68badae10f756ae69ca3
Pierre-Marie Achart <pierremarie.achart@gmail.com>
2018-08-09 19:03:12 +0200

    sauce

--- .fzf.bash
+++ .fzf.bash
@@ -15,5 +15,5 @@ source "/home/pma/.fzf/shell/key-bindings.bash"
 export FZF_DEFAULT_COMMAND='rg --hidden --files --sort-files'
 export FZF_DEFAULT_OPTS='--reverse --border --height 33%'
 
-export FZF_CTRL_T_COMMAND='rg --hidden --files --sort-files'
+export FZF_CTRL_T_COMMAND='rg --hidden --files'
 export FZF_CTRL_T_OPTS="--preview-window right:60% --preview 'head -n20 {} | pygmentize -g 2> /dev/null || echo {}'"
