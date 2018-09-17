1587c3ac8eeee56db4aa6f1783f328685766fa19
Pierre-Marie Achart <pierremarie.achart@gmail.com>
2018-09-17 11:38:35 +0200

    master : revamped tmux startup script with multi environment capability

--- tmuxinit.sh
+++ tmuxinit.sh
@@ -1,30 +1,66 @@
 #!/bin/bash
 
-if [[ $MYENV == 't460p' ]] ; then
+tmuxwindow() {
+  tmux rename-window "${NAME}"
 
-tmux new-session -d -s wynd -c '/home/pma/git/mellisuge'
+  tmux send-keys "cd ${DIR} && tb" 'C-m'
 
-tmux send-keys 'tb' 'C-m'
-tmux select-window -t ofs:1
+  tmux split-window -h -c "${DIR}" -p 30
+  tmux send-keys "${TOPCMD}" 'C-m'
 
-tmux split-window -h -c "#{pane_current_path}" -p 20
-tmux send-keys 'dcdev' 'C-m'
+  tmux split-window -v -t 2 -c "${DIR}" -l "${BTMCMDH}"
+  tmux send-keys "${BTMCMD}" 'C-m'
 
-tmux split-window -v -t 2 -c "#{pane_current_path}" -p 6
-tmux send-keys 'dcw' 'C-m'
+  tmux split-window -h -t 1 -c "${DIR}" -p 50
+  tmux send-keys 'git willpull ; g' 'C-m'
 
-tmux split-window -h -t 1 -c "#{pane_current_path}" -p 40
-tmux send-keys 'git willpull ; g' 'C-m'
+  tmux split-window -v -t 1 -c "${DIR}" -l 3
+  tmux send-keys 'yesterday '
+}
 
-tmux split-window -v -t 1 -c "#{pane_current_path}" -p 4
-tmux send-keys 'yesterday'
+tmuxinit() {
+  local NAME
+  local DIR
+  local TOPCMD
+  local BTMCMD
+  local BTMCMDH
 
-#tmux select-pane -t 2
-tmux attach -t wynd
+  if [[ -z ${TMUX} ]] ; then
+    echo 'Not in a tmux session, please create or attach to one. Exiting.'
+    exit 1
+  fi
 
-else
+  if [[ ${1} == '-n' ]] ; then
+    shift
+    tmux new-window
+  fi
 
-tmux
+  case ${1} in
+  mel*)
+    NAME='MELLI'
+    DIR='/home/pma/git/mellisuge'
+    TOPCMD='dcdev'
+    BTMCMD='dcw'
+    BTNCMDH='6'
+    tmuxwindow
+    ;;
+  boo*)
+    NAME='BOOKING'
+    DIR='/home/pma/git/BookingBundle'
+    TOPCMD='yarn start'
+    BTMCMD='htop'
+    BTMCMD='9'
+    tmuxwindow
+    ;;
+  *)
+    tmux split-window -h -p 50
+    tmux send-keys 'tb' 'C-m'
+    tmux split-window -v -t 2 -l 9
+    tmux send-keys 'htop' 'C-m'
+    tmux select-pane -t 1
+    ;;
+  esac
 
-fi
+}
 
+tmuxinit "$@"
