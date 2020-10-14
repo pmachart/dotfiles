358b6f1a5fecd8e306dbe5549dd02e0d6d66125d
Pierre-Marie Achart <pierremarie.achart@gmail.com>
2020-10-14 09:05:04 +0200

    status bar stuff

--- .config/i3blocks/btmousebattery.sh
+++ .config/i3blocks/btmousebattery.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ '/usr/bin/upower -d | grep --quiet mouse_dev_E8_1C_E1_42_49_27' ]] ; then 
-  echo -n "<span color='green'>Arc:"
+echo -n "Arc:"
+if [[ -n "$(/usr/bin/upower -d | grep mouse_dev_E8_1C_E1_42_49_27)" ]] ; then 
+  echo -n "<span color='green'>"
   echo -n $(/usr/bin/upower -i /org/freedesktop/UPower/devices/mouse_dev_E8_1C_E1_42_49_27 | grep percentage | awk '{print $2}')
   echo "</span>"
+else
+  echo -n "<span color='red'>Off</font>"
 fi
--- .config/i3blocks/icanhasvpn.sh
+++ .config/i3blocks/icanhasvpn.sh
@@ -4,6 +4,4 @@ if [[ -d "/proc/sys/net/ipv4/conf/tun0" ]] ; then
   echo -n "<span color='green'>VPN ("
   echo -n $(nmcli -t connection show --active | \grep :vpn: | awk -F ':' '{print $1}')
   echo ")</span>"
-else
-  echo "<span color='red'>off</span>"
 fi
